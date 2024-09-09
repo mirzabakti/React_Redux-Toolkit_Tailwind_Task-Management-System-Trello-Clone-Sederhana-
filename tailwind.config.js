7e0ebc4d5153763e4bf337c77870face42a29afa
mirzabakti <mirza.bakti@gmail.com>
2024-09-09 10:17:48 +0700

    resolve tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  purge: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'], // Tentukan file yang akan di-scan Tailwind
-  darkMode: false, // atau 'media' atau 'class'
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
