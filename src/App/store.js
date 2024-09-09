cf3fb4c8bac9006535d71a3a5008a7b334cd795c
mirzabakti <mirza.bakti@gmail.com>
2024-09-09 08:54:55 +0700

    setup redux store

---
+++ src/App/store.js
@@ -0,0 +1,10 @@
+// src/app/store.js
+import { configureStore } from '@reduxjs/toolkit';
+import tasksReducer from '../features/tasks/tasksSlice';
+
+// Buat store Redux dan masukkan reducer untuk tasks
+export const store = configureStore({
+  reducer: {
+    tasks: tasksReducer, // Tambahkan tasks reducer
+  },
+});
